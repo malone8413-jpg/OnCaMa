1e059332dca804e2a4efa9e2d8f22e23147c6878
Mevi <malone8413@gmail.com>
2026-04-17 21:10:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  base: "/OnCaMa/",
-  logLevel: "error",
   plugins: [react()],
+  base: '/OnCaMa/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
