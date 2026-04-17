12f194ced132bc958988a54064e515216b6ac54d
Mevi <malone8413@gmail.com>
2026-04-17 22:38:29 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-window.jsapiLocal = {};
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from '@/App.jsx'
 import '@/index.css'
+
+window.jsapiLocal = {}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
