eb91e278b47d965a483c7d3e68afbde0fbe27529
Mevi <malone8413@gmail.com>
2026-04-17 23:12:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/OnCaMa/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/OnCaMa/',   // ← C'est ça qui manque !
 })
